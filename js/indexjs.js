a7cd4827d6c92de344e200ffa57375758c5964b4
XFpzl <85889945@qq.com>
2017-05-08 04:18:14 +0800

    关注页面
    完成关注页面未登陆提示

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,3 +1,4 @@
+//初始化
 $(document).ready(function(){
 	//定义content的滚动高度
 	var temp=window.innerHeight-60;
@@ -7,5 +8,10 @@ $(document).ready(function(){
 	var mSlider=mui("#mslider");
 	mSlider.slider({
 		interval: 5000
+	});
+	//侧滑容器父节点
+	var offCanvasWrapper = mui('#offCanvasWrapper');
+	document.getElementById('userPic').addEventListener('tap', function() {
+		offCanvasWrapper.offCanvas('show');
 	});
 })

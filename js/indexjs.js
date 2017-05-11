8dbdba3c636e0b17815704a7cb5baa909d3d87ae
XFpzl <85889945@qq.com>
2017-05-12 00:22:45 +0800

    tab切换JS判断
    tab切换JS判断

--- js/indexjs.js
+++ js/indexjs.js
@@ -14,4 +14,23 @@ $(document).ready(function(){
 	document.getElementById('userPic').addEventListener('tap', function() {
 		offCanvasWrapper.offCanvas('show');
 	});
+	
+	var item1 = document.getElementById('item1mobile');
+	var item3 = document.getElementById('item3mobile');
+	var item3 = document.getElementById('item4mobile');
+	var item3 = document.getElementById('item5mobile');
+	var item3 = document.getElementById('item6mobile');
+	document.getElementById('slider').addEventListener('slide', function(e) {
+		if (e.detail.slideNumber === 0) {
+
+		} else if (e.detail.slideNumber === 2) {
+
+		} else if (e.detail.slideNumber === 3) {
+
+		} else if (e.detail.slideNumber === 4) {
+
+		} else if (e.detail.slideNumber === 5) {
+
+		}
+	});
 })
